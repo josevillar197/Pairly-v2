9a4af0a2238cb803f0a6ef9bc6eeefbd180306ce
Jose Villar <jaydvillar@gmail.com>
2026-01-23 16:37:37 +0100

    Authoritative frontend snapshot

--- src/pages/DiscoveryPage.jsx
+++ src/pages/DiscoveryPage.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
 import MatchCard from "../components/MatchCard";
-import LikeButton from "../components/LikeButton";
 import FilterBar from "../components/FilterBar";
-import EmptyState from "../components/EmptyState";
 
 function DiscoveryPage() {
-  const users = [
-    { id: 1, name: "Alex", tastes: ["Movies", "Gaming"] },
-    { id: 2, name: "Jamie", tastes: ["Shows", "Indie Games"] },
-  ];
-
-  const [index, setIndex] = useState(0);
-  const currentUser = users[index];
-
   return (
     <div className="page discover-page">
-      <h1>Discover</h1>
-
       <FilterBar />
 
-      {currentUser ? (
-        <>
-          <MatchCard user={currentUser} />
-          <LikeButton onLike={() => setIndex(index + 1)} />
-        </>
-      ) : (
-        <EmptyState />
-      )}
+      <div className="discover-feed">
+        <MatchCard />
+      </div>
     </div>
   );
 }
